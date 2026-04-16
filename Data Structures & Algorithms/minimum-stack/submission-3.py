2ce41ae550bc47926c54f90c9e89da39d7d14120
Shreyas Shinde <shreyas.shinde9419@gmail.com>
2026-04-15 19:25:58 -0700

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = list()
+        self.currentMin = float('inf')
+        
+
+    def push(self, val: int) -> None:
+        self.currentMin = min(val, self.currentMin)
+        self.stack.append([val, self.currentMin])
+
+    def pop(self) -> None:
+        self.stack.pop()
+        if self.stack:
+            self.currentMin = self.stack[-1][1]
+        else:
+            self.currentMin = float('inf')
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+        
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+
+        
